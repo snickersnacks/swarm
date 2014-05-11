d734c97227ce457ef2703e80106b0f89b3ddeed1
zite <zite00@gmail.com>
2014-05-10 23:21:49 -0700

    aiplacement fix

--- Swarm/Assets/Scripts/AIPlacement.cs
+++ Swarm/Assets/Scripts/AIPlacement.cs
@@ -16,22 +16,22 @@ public class AIPlacement : MonoBehaviour
 	public Vector2 max;
 	public float step;
 
-<<<<<<< HEAD
-	public Shader MapGradient;
-=======
 	public Shader HeatMapShader;
 
 	private int towerlayer;
 	private int rangelayer;
->>>>>>> cc1826396c5db3f2b8afdb6b71d49f390935585f
 
     private List<LineSegment> linesegments = new List<LineSegment>();
     private List<PotentialTower> potentialTowers = new List<PotentialTower>();
+
+    private Transform potentialTowerParent;
+
 	void Awake()
 	{
 		i = this;
 		towerlayer = LayerMask.NameToLayer("PositioningAITower");
 		rangelayer = LayerMask.NameToLayer("PositioningAIRange");
+        potentialTowerParent = new GameObject("[DEBUG] Potential Tower Parent").transform;
 	}
 
 
@@ -41,15 +41,6 @@ public class AIPlacement : MonoBehaviour
 		{
 			GetSegments(startingnode);
 		}
-
-        foreach (LineSegment segment in linesegments)
-        {
-            GameObject line = new GameObject(segment.StartNodeGO.name + " to " + segment.EndNodeGO.name);
-            LineRenderer lr = line.AddComponent<LineRenderer>();
-            lr.SetWidth(0.05f, 0.05f);
-            lr.SetPosition(0, new Vector3(segment.StartPos.x, segment.StartPos.y, -1));
-            lr.SetPosition(1, new Vector3(segment.EndPos.x, segment.EndPos.y, -1));
-        }
 	}
 
 	private void GetSegments(GameObject fromnode)
@@ -66,13 +57,26 @@ public class AIPlacement : MonoBehaviour
 		}
 	}
 
-
+    bool showdebug = false;
 	void Update()
 	{
 		if (Input.GetKeyUp(KeyCode.B))
 		{
 			StartCoroutine(DoThing());
 		}
+
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            showdebug = !showdebug;
+
+            foreach (LineSegment segment in linesegments)
+                segment.DebugLine.enabled = showdebug;
+
+            foreach (PotentialTower pTower in potentialTowers)
+            {
+                pTower.gameObject.renderer.enabled = false;
+            }
+        }
 	}
 
 	public IEnumerator DoThing()
@@ -99,12 +103,14 @@ public class AIPlacement : MonoBehaviour
 		{
 			for (float y = min.y; y < max.y; y+=step)
 			{
-				GameObject rangeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				DestroyImmediate(rangeGO.GetComponent<Collider>());
+                GameObject rangeGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                DestroyImmediate(rangeGO.GetComponent<Collider>());
 				CircleCollider2D rangeCollider = rangeGO.AddComponent<CircleCollider2D>();
 				rangeCollider.radius = rangeRadius;
 				rangeCollider.isTrigger = true;
-				rangeGO.layer = rangelayer;
+                rangeGO.layer = rangelayer;
+                rangeGO.transform.parent = potentialTowerParent;
+                rangeGO.renderer.material.shader = HeatMapShader;
 
 				Vector2 currentpos = new Vector2(x, y);
 				rangeGO.transform.position = new Vector3(x, y, 0);
@@ -114,33 +120,33 @@ public class AIPlacement : MonoBehaviour
                 
                 distsdists(potentialTower, currentpos, rangeRadius, towerRadius);
 
+                potentialTowers.Add(potentialTower);
+
                 if (potentialTower.TotalDistance > 0)
 				{
-<<<<<<< HEAD
-					positions.Add(currentpos, totaldist);
-					rangeGO.name = totaldist.ToString();
-					rangeGO.renderer.material.shader = MapGradient;
-					rangeGO.renderer.material.color = new Color(totaldist / 20f, 0, 0, 0.7f);
-=======
-                    potentialTowers.Add(potentialTower);
-
                     rangeGO.name = potentialTower.TotalDistance.ToString();
-					rangeGO.renderer.material.shader = HeatMapShader;
-                    rangeGO.renderer.material.color = new Color(potentialTower.TotalDistance / 30f, 0, 0, 0.85f);
->>>>>>> cc1826396c5db3f2b8afdb6b71d49f390935585f
+                    rangeGO.renderer.material.color = new Color(potentialTower.TotalDistance / 26f, 0, 0, 0.85f);
 				}
 				else
                 {
-                    rangeGO.name = "death";
-					rangeGO.renderer.material.shader = HeatMapShader;
                     if (potentialTower.TotalDistance == (int)AIPLACEMENTERRORS.NodeTouchingTower)
-                        rangeGO.renderer.material.color = Color.yellow; // no segments touching range
+                    {
+                        rangeGO.name = "Node is touching tower";
+                        rangeGO.renderer.material.color = Color.yellow; 
+                    }
                     else if (potentialTower.TotalDistance == (int)AIPLACEMENTERRORS.PathOutsideRange)
-                        rangeGO.renderer.material.color = Color.blue; // tower inside node
+                    {
+                        rangeGO.name = "Path is outside the range of the tower";
+                        rangeGO.renderer.material.color = Color.blue; 
+                    }
                     else if (potentialTower.TotalDistance == (int)AIPLACEMENTERRORS.TowerInPath)
-                        rangeGO.renderer.material.color = Color.green; // tower in path
+                    {
+                        rangeGO.name = "Tower is in the path";
+                        rangeGO.renderer.material.color = Color.green;
+                    }
                     else if (potentialTower.TotalDistance == 0)
                     {
+                        rangeGO.name = "_____ERROR TOWN_____";
                         Debug.Log("This really shouldn't happen. The potential tower is touching the path, but... no... amount of distance on the path.");
                         rangeGO.renderer.material.color = Color.cyan; // no... distance?
                     }
@@ -243,12 +249,16 @@ public enum AIPLACEMENTERRORS
 
 public class LineSegment
 {
+    public static Transform parent;
+
 	public GameObject StartNodeGO;
     public GameObject EndNodeGO;
     public NextNode StartNodeScript;
     public NextNode EndNodeScript;
     public Vector2 StartPos;
     public Vector2 EndPos;
+
+    public LineRenderer DebugLine;
 
     public LineSegment(GameObject start, GameObject end)
     {
@@ -260,6 +270,18 @@ public class LineSegment
 
         StartPos = new Vector2(start.transform.position.x, start.transform.position.y);
         EndPos = new Vector2(end.transform.position.x, end.transform.position.y);
+
+        if (parent == null)
+        {
+            parent = new GameObject("[DEBUG] Line segment parent").transform;
+        }
+
+        GameObject line = new GameObject(this.StartNodeGO.name + " to " + this.EndNodeGO.name);
+        line.transform.parent = parent;
+        DebugLine = line.AddComponent<LineRenderer>();
+        DebugLine.SetWidth(0.05f, 0.05f);
+        DebugLine.SetPosition(0, new Vector3(this.StartPos.x, this.StartPos.y, -1));
+        DebugLine.SetPosition(1, new Vector3(this.EndPos.x, this.EndPos.y, -1));
     }
 
 	public override bool Equals(System.Object obj)
